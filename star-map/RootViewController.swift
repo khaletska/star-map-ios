d2c41c5fb10c8f99a64b714a4d3cd8b88f58ab87
Andrius Shiaulis <shiaulis@gmail.com>
2024-06-01 01:33:24 +0300

    Add ARview

--- star-map/RootViewController.swift
+++ star-map/RootViewController.swift
@@ -6,13 +6,41 @@
 //
 
 import UIKit
+import ARKit
 
 final class RootViewController: UIViewController {
+
+    // MARK: - Properties -
+
+    private let augmentedRealityView: ARSCNView
+
+    // MARK: - Init -
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        self.augmentedRealityView = .init(frame: .zero)
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - View Controller Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.backgroundColor = .systemOrange
+    }
+
+    // MARK: - Private API -
+
+    private func setupViews() {
+        self.augmentedRealityView.frame = self.view.bounds
+        self.augmentedRealityView.backgroundColor = .systemRed
+        self.view.addSubview(self.augmentedRealityView)
     }
 
 }
